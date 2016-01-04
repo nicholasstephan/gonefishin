8e44406a43458c32f9e1b181dcbfcff0f790eb66
Nicholas Stephan <nicholas.stephan@gmail.com>
2016-01-03 18:04:52 -0800

    mapview

--- index.ios.js
+++ index.ios.js
@@ -10,22 +10,18 @@ var {
   StyleSheet,
   Text,
   View,
+  MapView,
 } = React;
 
 var GoneFishin = React.createClass({
+
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <MapView
+          style={styles.map}
+          showsUserLocation={true}
+          />
       </View>
     );
   }
@@ -34,20 +30,13 @@ var GoneFishin = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'green',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+
+  map: {
+    flex: 1,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
 
 AppRegistry.registerComponent('GoneFishin', () => GoneFishin);
